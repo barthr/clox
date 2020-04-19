dd6035a91214d1371994490bcf103124acfe6d97
Bart <info@bartfokker.com>
2020-04-19 13:49:22 +0200

    add basis of allocating and reallocating chunks

--- chunk.h
+++ chunk.h
@@ -17,6 +17,8 @@ typedef struct
 
 void initChunk(Chunk *chunk);
 
+void freeChunk(Chunk *chunk);
+
 void writeChunk(Chunk *chunk, uint8_t byte);
 
 #endif
--- memory.h
+++ memory.h
@@ -8,6 +8,9 @@
     (type *)reallocate(previous, sizeof(type) * (oldCount), \     
         sizeof(type) * (count))
 
+#define FREE_ARRAY(type, pointer, oldCount) \
+    reallocate(pointer, sizeof(type) * oldCount, 0)
+
 void *reallocate(void *previous, size_t oldSize, size_t newSize);
 
 #endif
