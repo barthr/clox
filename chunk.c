3fa41e31fa803206dd088b4f5652804e6b4087cf
Bart <info@bartfokker.com>
2020-04-19 14:57:36 +0200

    free memory of value array on freeing chunk

--- chunk.c
+++ chunk.c
@@ -15,6 +15,7 @@ void initChunk(Chunk *chunk)
 void freeChunk(Chunk *chunk)
 {
     FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+    freeValueArray(&chunk->constants);
     initChunk(chunk);
 }
 
